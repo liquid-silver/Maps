460b91347bc1431a062a84ca80d7d0105b485025
Полина Коновалова <konovalova.pv@edu.spbstu.ru>
2023-02-19 12:47:59 +0300

    first

---
+++ MapsAPI/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace MapsAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
